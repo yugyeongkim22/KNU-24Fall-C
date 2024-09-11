8fa65319bf2f17523719d013d8f480b254815707
yugyeongkim22 <yugyeong020222@gmail.com>
2024-09-11 12:40:12 +0900

    Hello, World

--- day01/day01-1.c
+++ day01/day01-1.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-	printf("Hello, World\n");
-<<<<<<< HEAD
-	printf("ù ������Ʈ");
-=======
-	printf("Ã¹ ÇÁ·ÎÁ§Æ®");
->>>>>>> eccad8dce355ca1e05a72998247c22a54f333185
+int main() {
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
 
 	return 0;
 }
