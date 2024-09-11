4e6f4871374fe5d4cbadc147a209491dc5225901
yugyeongkim22 <yugyeong020222@gmail.com>
2024-09-11 12:28:07 +0900

    Hello, World

---
+++ day01/day01-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello, World\n");
+	printf("첫 프로젝트");
+
+	return 0;
+}
--- day01/day01-2.c
+++ day01/day01-2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
     int i, j, n;
@@ -26,6 +26,7 @@ int main() {
 
 
 }
+*/
 
 
 
