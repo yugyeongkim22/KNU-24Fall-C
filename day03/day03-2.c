6f0c950c2841cc78409f0974a86abd14c2085603
yugyeongkim22 <yugyeong020222@gmail.com>
2024-09-19 13:23:24 +0900

    다중 반복문 실습
    피라미드

---
+++ day03/day03-2.c
@@ -0,0 +1,25 @@
+/*#include <stdio.h>
+
+// 재귀 함수로 팩토리얼 계산
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;  // 0! = 1, 1! = 1
+    }
+    else {
+        return n * factorial(n - 1);  // n! = n * (n-1)!
+    }
+}
+
+int main() {
+    int number;
+
+    // 사용자 입력 받기
+    printf("계산할 팩토리얼 값 입력: ");
+    scanf_s("%d", &number);
+
+    // 팩토리얼 계산 및 결과 출력
+    printf("%d! = %d\n", number, factorial(number));
+
+    return 0;
+}
+*/
