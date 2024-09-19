b6e4a364879e90bee65188b4c63b166124e9ff67
yugyeongkim22 <yugyeong020222@gmail.com>
2024-09-19 14:19:15 +0900

    함수 실습
    소수 판단

--- day03/day03-1.c
+++ day03/day03-1.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 
-
-int is_prime(int num) {
-    if (num <= 1) {
-        return 0; 
-    }
-
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return 0; 
-        }
-    }
-
-    return 1; 
+int isPrimeNumber(int num)
+{
+	for (int i = 2; i < num; i++) {
+		if (num % i == 0) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
+int main()
+{
+	int num;
+	scanf_s("%d", &num);
+	int res = isPrimeNumber(num);
 
-int main() {
-    int number;
+	printf("%d\n", res);
 
-   
-    printf("소수인지 확인할 수를 입력하세요: ");
-    scanf_s("%d", &number);
-
-   
-    if (is_prime(number)) {
-        printf("해당 수는 소수입니다.\n");
-    }
-    else {
-        printf("해당 수는 소수가 아닙니다.\n");
-    }
-
-    return 0;
 }
