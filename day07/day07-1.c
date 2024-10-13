dfc64106b245de857c42745fee3fd9a356913b1c
yugyeongkim22 <yugyeong020222@gmail.com>
2024-10-13 17:51:30 +0900

    피라미드

--- day07/day07-1.c
+++ day07/day07-1.c
@@ -2,27 +2,28 @@
 
 int main() {
 
-	int floor; // 피라미드의 층 수를 저장할 변수 선언
-	scanf_s("%d", &floor); // 사용자로부터 층 수를 입력 받음
-	printf("%d층 피라미드\n", floor); // 입력 받은 층 수를 출력
+	int floor; 
+	printf("몇 층으로 만드시겠습니까?");
+	scanf_s("%d", &floor); 
+	
 
-	// i는 현재 층, 0층부터 시작
+
 	for (int i = 0; i < floor; i++)
 	{
-		// j는 공백을 출력, 각 층에 대해 공백을 출력하여 피라미드 모양을 맞춤
+		
 		for (int j = i; j < floor - 1; j++)
 		{
-			printf(" "); // 왼쪽에 필요한 공백을 출력
+			printf(" "); 
 		}
 
-		// k는 각 층에 찍힐 '*'의 개수
+		
 		for (int k = 0; k < i * 2 + 1; k++)
 		{
-			printf("*"); // '*'을 출력하여 피라미드 만들기
+			printf("*"); 
 		}
-		printf("\n"); // 한 층이 끝나면 다음 줄로
+		printf("\n"); 
 	}
 
-	return 0; // 프로그램 종료
+	return 0; 
 }
 
